85aa4211891d5765766029981ba09f0f9b6da86c
ankit120490 <88392616+ankit120490@users.noreply.github.com>
2021-08-04 02:36:20 +0900

    Update Constants.php
    path of sample file update

--- src/Constants.php
+++ src/Constants.php
@@ -4,7 +4,7 @@ class Constants {
   const bankAccountNumberMissingMessage = 'Bank account number missing';
   const bankBranchCodeMissingMessage = 'Bank branch code missing';
   const bankEndToEndIDMissingMessage = 'End to end id missing';
-  const sampleCsvName = 'data_sample.csv';
+  const sampleCsvName = 'tests/support/data_sample.csv';
   
   const fileErrorCode = 150; //Custom Error Code. Can be changed according to the code repository standards
   const fileErrorDocumentName = 'No FileName';
