aa1805924d7c4a0187ae6a0cd46641ffe573870d
ankit120490 <88392616+ankit120490@users.noreply.github.com>
2021-08-04 01:24:25 +0900

    Create Constants.php
    Adding new constant file for removing hard coded message on main file

---
+++ src/Constants.php
@@ -0,0 +1,10 @@
+<php?
+      
+class Constants {
+  const bankAccountNumberMissingMessage = 'Bank account number missing';
+  const bankBranchCodeMissingMessage = 'Bank branch code missing';
+  const bankEndToEndIDMissingMessage = 'End to end id missing';
+  const sampleCsvName = 'data_sample4.csv';
+      
+}
+ ?>
