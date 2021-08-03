560fa2378b5a19e2c17dc5955067df956a4803e6
ankit120490 <88392616+ankit120490@users.noreply.github.com>
2021-08-04 02:57:25 +0900

    Update Constants.php
    Adding general failure code and message

--- src/Constants.php
+++ src/Constants.php
@@ -4,7 +4,10 @@ class Constants {
   const bankAccountNumberMissingMessage = 'Bank account number missing';
   const bankBranchCodeMissingMessage = 'Bank branch code missing';
   const bankEndToEndIDMissingMessage = 'End to end id missing';
-  const sampleCsvName = 'tests/support/data_sample.csv';
+  const sampleCsvPath = 'tests/support/data_sample.csv';
+  
+  const generalFailureCode = 100;
+  const generalFailureMessage = 'All systems go';
   
   const fileErrorCode = 150; //Custom Error Code. Can be changed according to the code repository standards
   const fileErrorDocumentName = 'No FileName';
