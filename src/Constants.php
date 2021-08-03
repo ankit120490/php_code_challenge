8c217641a85710db6de1c0a310a1aef2de64d29d
ankit120490 <88392616+ankit120490@users.noreply.github.com>
2021-08-04 01:26:04 +0900

    Update Constants.php
    Message added

--- src/Constants.php
+++ src/Constants.php
@@ -1,10 +1,14 @@
-<php?
+<?php
       
 class Constants {
   const bankAccountNumberMissingMessage = 'Bank account number missing';
   const bankBranchCodeMissingMessage = 'Bank branch code missing';
   const bankEndToEndIDMissingMessage = 'End to end id missing';
-  const sampleCsvName = 'data_sample4.csv';
-      
+  const sampleCsvName = 'data_sample.csv';
+  
+  const fileErrorCode = 150;
+  const fileErrorDocumentName = 'No FileName';
+
 }
- ?>
+
+?>
